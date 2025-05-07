7867c65b497b9a0cac7a3f4a5beb62c974699718
choldgraf <choldgraf@gmail.com>
2025-05-06 19:40:54 -0700

    mkdir

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -7,5 +7,6 @@ text = path_template.read_text()
 for org in orgs:
     text_org = text.replace("{{ org }}", org)
     path_org = here / ".." / "book" / "org" / f"{org}.md"
+    path_org.mkdir(parents=True, exist_ok=True)
     path_org.write_text(text_org)
 print(f"Finished generating {len(orgs)} org pages...")
